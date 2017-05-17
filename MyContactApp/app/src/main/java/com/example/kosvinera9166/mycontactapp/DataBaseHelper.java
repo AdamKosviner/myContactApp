453b93bf34587ea33c1c8f38928624a26c6c1cfc
AdamKosviner <adamkosviner@gmail.com>
2017-05-17 09:21:35 -0700

    Since last update added contacts box, improved storage, more
    Added contacts box, address and age box, started working on data
    implementation system for new boxes, continued work on criteria listed
    on classroom

--- MyContactApp/app/src/main/java/com/example/kosvinera9166/mycontactapp/DataBaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/kosvinera9166/mycontactapp/DataBaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.kosvinera9166.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,10 +10,13 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by kosvinera9166 on 5/11/2017.
  */
 public class DataBaseHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "contact.db";
+    public static final String DATABASE_NAME = "Contact.db";
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1="ID";
     public static final String COL_2="NAME";
+    public static final String COL_3="ADDRESS";
+    public static final String COL_4="AGE";
+
     public DataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
@@ -26,4 +31,21 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
+    public boolean insertData (String name){
+        SQLiteDatabase db =this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+
+        long result =db.insert(TABLE_NAME,null,contentValues);
+        if (result == -1) return false;
+        else return true;
+    }
+
+    public Cursor getAllData(){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res =db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
+    }
 }
+
--- MyContactApp/app/src/main/java/com/example/kosvinera9166/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/kosvinera9166/mycontactapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.kosvinera9166.mycontactapp;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,5 +21,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DataBaseHelper(this);
+        editName=(EditText) findViewById(R.id.editText_name);
     }
+
+
+
+
+    public void addData (View v){
+
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+
+        if (isInserted==true){
+            Log.d("My Contact","Data insertion successful");
+            //text telling user data inserteded
+        }
+        else{
+            Log.d("My Contact","Data insertion unsuccessful");
+            //toast nessage indicating data wrongly imputed
+        }
+    }
+    public void viewData(View v){
+        Cursor res =myDb.getAllData();
+        if (res.getCount()==0){
+            showMessage("Error","No data found in database");
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        //set up while loop with Cursor move to next method por favor
+        //append each column to the buffer
+        //use getString method
+        showMessage("Data", buffer.toString());
+    }
+
+    private void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+
 }
